42c5810f4abc33e02ab1228b4dfaef268bf4fc9d
Vishwas Acharya <vishwasracharya@gmail.com>
2022-06-24 11:25:59 +0530

    tmp added

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 exports.site_url = process.env.site_url || "http://localhost:3002";
 // exports.api_url = process.env.api_url || "http://localhost:3001";
-exports.uploadDir = __dirname.replace(/\\/g, '/') + "/videos/";
+exports.uploadDir = __dirname.replace(/\\/g, '/') + "/tmp/";
 exports.publicDir = __dirname.replace(/\\/g, '/') + "/public/";
